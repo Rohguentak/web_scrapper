5fb72865aad57906e73fc52aa6faf7add69333be
Rohguentak <rhtg78@gmail.com>
2021-02-04 06:16:52 +0000

    get job title from indeed

--- indeed.py
+++ indeed.py
@@ -6,7 +6,7 @@ INDEED_URL=f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={JOB_NUM_LI
 
 def extract_indeed_pages():
   i = 1
-  temp=0
+  temp=1
   while(i == 1):
     result = requests.get(f"{INDEED_URL}&start={temp*JOB_NUM_LIMIT}")
     soup = BeautifulSoup(result.text, 'html.parser')
@@ -17,14 +17,11 @@ def extract_indeed_pages():
     for l in links:
       if (l.get('aria-label') == '다음'):
         i = 1 
-        print(i)
-        print(l.get('aria-label'))
-        print(temp)
       else :
         i = 0
     temp = temp + 1
 
-  return temp + 1
+  return temp
   #pages = []
   #for link in links[0:-1]:
   #  pages.append(int(link.string))
@@ -37,6 +34,12 @@ def extract_indeed_jobs(last_page):
   jobs = []
   for page in range(last_page):
     result = requests.get(f"{INDEED_URL}&start={page*JOB_NUM_LIMIT}")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    job_div = soup.find_all("div",{"class": "jobsearch-SerpJobCard"})
+ 
+    for d in job_div:
+      title = d.find("h2",{"class":"title"}).find("a")["title"]
+      print(title)
     print(result.status_code)
   return jobs
     
