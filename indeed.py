f5c706c3a6a07f703f4221b8af8d6aaeff962bc8
Rohguentak <rhtg78@gmail.com>
2021-02-04 05:44:20 +0000

    change code cause web page source is changed to max page 5

--- indeed.py
+++ indeed.py
@@ -5,17 +5,32 @@ JOB_NUM_LIMIT = 50
 INDEED_URL=f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={JOB_NUM_LIMIT}"
 
 def extract_indeed_pages():
-  result = requests.get(INDEED_URL)
-  soup = BeautifulSoup(result.text, 'html.parser')
+  i = 1
+  temp=0
+  while(i == 1):
+    result = requests.get(f"{INDEED_URL}&start={temp*JOB_NUM_LIMIT}")
+    soup = BeautifulSoup(result.text, 'html.parser')
 
-  pagination = soup.find("div", {"class":"pagination"})
-  links = pagination.find_all('a')
-  pages = []
-  for link in links[0:-1]:
-    pages.append(int(link.string))
+    pagination = soup.find("div", {"class":"pagination"})
+    links = pagination.find_all('a')
+    
+    for l in links:
+      if (l.get('aria-label') == '다음'):
+        i = 1 
+        print(i)
+        print(l.get('aria-label'))
+        print(temp)
+      else :
+        i = 0
+    temp = temp + 1
 
-  max_page = pages[-1]
-  return max_page
+  return temp + 1
+  #pages = []
+  #for link in links[0:-1]:
+  #  pages.append(int(link.string))
+
+  #max_page = pages[-1]
+  #return max_page
 
 
 def extract_indeed_jobs(last_page):
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 from indeed import extract_indeed_pages,extract_indeed_jobs
 
 last_indeed_pages=extract_indeed_pages()
-extract_indeed_jobs(last_indeed_pages)
+print(last_indeed_pages)
+#extract_indeed_jobs(last_indeed_pages)
