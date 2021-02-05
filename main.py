7f1c437cdb37a5e0bc38ce996f3df323e23ed164
Rohguentak <rhtg78@gmail.com>
2021-02-05 04:20:06 +0000

    update requirements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from indeed import get_jobs as get_indeed_jobs
 from stackoverflow import get_jobs as get_SO_jobs
-
+from save import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 SO_jobs = get_SO_jobs()
@@ -10,3 +10,4 @@ SO_jobs = get_SO_jobs()
 jobs = indeed_jobs + SO_jobs
 
 print(jobs)
+save_to_file(jobs)
---
+++ save.py
@@ -0,0 +1,6 @@
+import csv
+
+def save_to_file(jobs):
+  file = open("job_info.csv", mode="w")
+  print(file)
+  return file
